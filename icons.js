6c02a6e72d90605e8df09fdbfbb65e853074bb4e
corngoblin <81638743+corngoblin@users.noreply.github.com>
2026-04-30 09:35:07 -0400

    Refactor icon download and handling logic
    Refactor icon handling by removing curl and mogrify dependencies, and using Soup for image downloading. Update functions to streamline icon retrieval and saving.

--- icons.js
+++ icons.js
@@ -1,28 +1,12 @@
-import Gtk from 'gi://Gtk';
+import Soup from 'gi://Soup';
 import GLib from 'gi://GLib';
 import Gio from 'gi://Gio';
-import Gdk from 'gi://Gdk';
+import GdkPixbuf from 'gi://GdkPixbuf';
 
 const icons_path = GLib.get_user_cache_dir() + '/parasocial-extension';
 
-export function init_icons() {
-  GLib.mkdir_with_parents(icons_path, 448);
-
-  var display = Gdk.Display.get_default();
-  if (display == null) return;
-
-  var icon_theme = Gtk.IconTheme.get_for_display(display);
-  if (icon_theme == null) return;
-
-  icon_theme.add_search_path(icons_path);
-}
-
-function curl_available() {
-  return GLib.find_program_in_path('curl');
-}
-
-function mogrify_available() {
-  return GLib.find_program_in_path('mogrify');
+export function get_icon_name(streamername) {
+  return 'parasocial-' + streamername.toLowerCase();
 }
 
 export function get_final_icon_path(streamername) {
@@ -33,74 +17,35 @@ export function has_icon(streamername) {
   return GLib.file_test(get_final_icon_path(streamername), GLib.FileTest.EXISTS);
 }
 
+// Native download + conversion (no curl / mogrify)
 export function trigger_download_by_url(streamername, imageurl) {
   return new Promise((resolve, reject) => {
-    if (!curl_available()) {
-      resolve(false);
-      return;
-    }
-
     const final = get_final_icon_path(streamername);
     if (GLib.file_test(final, GLib.FileTest.EXISTS)) {
       resolve(true);
       return;
     }
 
-    let ext = imageurl.split('.').pop().split('?')[0].toLowerCase();
-    if (ext.length > 4) ext = 'png';
-
-    const temp = GLib.build_filenamev([icons_path, 'tmp_' + new Date().getTime() + '.' + ext]);
-
-    let curlCmd = ['curl', '-s', imageurl, '-o', temp];
-    let proc = Gio.Subprocess.new(
-      curlCmd,
-      Gio.SubprocessFlags.STDOUT_SILENCE | Gio.SubprocessFlags.STDERR_SILENCE
-    );
-
-    proc.wait_async(null, (proc, result) => {
+    const session = new Soup.Session();
+    const message = Soup.Message.new('GET', imageurl);
+    session.send_and_read_async(message, GLib.PRIORITY_DEFAULT, null, (s, r) => {
       try {
-        proc.wait_finish(result);
-        if (!proc.get_successful()) {
-          GLib.unlink(temp);
+        const bytes = s.send_and_read_finish(r).get_data();
+        if (!bytes || bytes.byteLength === 0) {
           resolve(false);
           return;
         }
 
-        let fileInfo = Gio.File.new_for_path(temp).query_info('standard::size', Gio.FileQueryInfoFlags.NONE, null);
-        if (fileInfo.get_size() === 0) {
-          GLib.unlink(temp);
+        const gBytes = GLib.Bytes.new(bytes);
+        const stream = Gio.MemoryInputStream.new_from_bytes(gBytes);
+        const pixbuf = GdkPixbuf.Pixbuf.new_from_stream(stream, null);
+        if (!pixbuf) {
           resolve(false);
           return;
         }
 
-        if (ext === 'png') {
-          Gio.File.new_for_path(temp).move(Gio.File.new_for_path(final), Gio.FileCopyFlags.OVERWRITE, null, null);
-          resolve(true);
-        } else if (mogrify_available()) {
-          let convCmd = ['mogrify', '-format', 'png', temp];
-          let convProc = Gio.Subprocess.new(
-            convCmd,
-            Gio.SubprocessFlags.STDOUT_SILENCE | Gio.SubprocessFlags.STDERR_SILENCE
-          );
-          convProc.wait_async(null, (convProc, res) => {
-            try {
-              convProc.wait_finish(res);
-              let pngTemp = temp.replace(/\.[^.]+$/, '.png');
-              if (GLib.file_test(pngTemp, GLib.FileTest.EXISTS)) {
-                Gio.File.new_for_path(pngTemp).move(Gio.File.new_for_path(final), Gio.FileCopyFlags.OVERWRITE, null, null);
-                if (GLib.file_test(temp, GLib.FileTest.EXISTS)) GLib.unlink(temp);
-                resolve(true);
-              } else {
-                resolve(false);
-              }
-            } catch (e) {
-              resolve(false);
-            }
-          });
-        } else {
-          GLib.unlink(temp);
-          resolve(false);
-        }
+        pixbuf.savev(final, 'png', [], []);
+        resolve(true);
       } catch (e) {
         resolve(false);
       }
@@ -108,18 +53,11 @@ export function trigger_download_by_url(streamername, imageurl) {
   });
 }
 
-export function get_icon_name(streamername) {
-  return 'parasocial-' + streamername.toLowerCase();
-}
-
 export function get_streamericon(streamername, style_class) {
   let icon = new imports.gi.St.Icon({
     gicon: Gio.icon_new_for_string(get_final_icon_path(streamername)),
     style_class: style_class
   });
-  if (icon.set_fallback_icon_name) {
-    icon.set_fallback_icon_name('parasocial');
-  }
   return icon;
 }
 
